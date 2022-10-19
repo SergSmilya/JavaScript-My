9f217056f930a1d96e16b671e092c96f4ac274d8
SergSmilya <sergsmilichenko@gmail.com>
2022-10-19 20:18:11 +0300

    Pause 49min.

---
+++ js/test.js
@@ -0,0 +1,17 @@
+const x = 10;
+const y = 5;
+console.log(x + 5);
+// window alert
+// alert (x*y);
+
+// const type = typeof true;
+// console.log(type);
+
+// const shouldRenew = confirm('Продовжити підписку?');
+// console.log(shouldRenew);
+
+// const або let
+let quantity = prompt('одиниці товару');
+quantity = Number(quantity);
+console.log(quantity);
+console.log(typeof quantity);
