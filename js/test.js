11807b9e090cb1bf8f3587f516376c0e40df8267
SergSmilya <sergsmilichenko@gmail.com>
2022-10-20 19:59:26 +0300

    auto

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
-const x = 10;
-const y = 5;
-console.log(x + 5);
+// const x = 10;
+// const y = 5;
+// console.log(x + 5);
 // window alert
 // alert (x*y);
 
@@ -11,7 +11,45 @@ console.log(x + 5);
 // console.log(shouldRenew);
 
 // const або let
-let quantity = prompt('одиниці товару');
-quantity = Number(quantity);
-console.log(quantity);
-console.log(typeof quantity);
+// let quantity = prompt('одиниці товару');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// function sayHi() {
+//   console.log("Hello, this is my first function!");
+// }
+// sayHi();
+
+// function add(a,b,c) {
+//   const results = a+b+c;
+//   console.log(`Addition result equals ${results}`);
+// }
+  
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+//     const message = `You ordered droids worth ${(orderedQuantity * pricePerDroid) + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+//     return message;
+//     console.log(message);
+// }
+
+function isAdult(age) {
+  // Change code below this line
+  const passed = isAdult >= 18  ;
+
+  // Change code above this line
+  return passed;
+}
+
