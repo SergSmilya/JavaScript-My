546d1e1c54ae929314e97bdea063898de5cfc598
SergSmilya <sergsmilichenko@gmail.com>
2022-11-16 17:33:42 +0200

    task 10/41

---
+++ js/hw_3.js
@@ -0,0 +1,92 @@
+// task 4/41
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags.pop();
+// console.log(lastTag) 
+// // Change code above this line
+
+// task 6/41
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+// apartment.price=5000;
+// apartment.rating=4.7;
+// apartment.owner.name="Henry Sibola";
+// apartment.tags.push("trusted");
+// console.log(apartment.tags)
+
+// task 8/41
+
+// const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   // Change code below this line
+//   name,
+//   price,
+//   image,
+//   tags,
+//   // Change code above this line
+   
+// };
+// console.log(product)
+
+// task 9/41
+
+// const emailInputName = "email";
+// const passwordInputName = "password";
+
+// const credentials = {
+//   // Change code below this line
+// [emailInputName]: "henry.carter@aptmail.com",
+// [passwordInputName]: "jqueryismyjam"
+
+//   // Change code above this line
+// };
+// console.log(credentials)
+
+
+// task 10/41
+                            ???????????????????????????
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+
+for (const key in apartment) {
+    console.log(key);
+    console.log(apartment[key]);
+};
