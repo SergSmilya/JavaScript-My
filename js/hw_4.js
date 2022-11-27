20f76eecff54e7f625eccd0f3fe3571c8dd01761
SergSmilya <sergsmilichenko@gmail.com>
2022-11-27 07:09:51 +0200

    hw_4.26

--- js/hw_4.js
+++ js/hw_4.js
@@ -28,3 +28,200 @@
 //   console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
 //   console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
 //   console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
+
+// task 14/48
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+// // Change code below this line
+// const planetsLengths = planets.map(element => element.length);
+// console.log(planetsLengths)
+
+// task 15/48
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       rating: 8.38,
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       rating: 8.51,
+//     },
+//     {
+//       title: "The Dream of a Ridiculous Man",
+//       author: "Fyodor Dostoevsky",
+//       rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//     { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+//   ];
+//   // Change code below this line
+  
+//   const titles = books.map(book=>book.title);
+  
+// console.log(titles);
+
+// task 16/48
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       genres: ["adventure", "history"],
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       genres: ["fiction"],
+//     },
+//     {
+//       title: "Redder Than Blood",
+//       author: "Tanith Lee",
+//       genres: ["horror", "mysticism"],
+//     },
+//   ];
+//   // Change code below this line
+  
+//   const genres = books.flatMap(book=>book.genres);
+//   console.log(genres)
+
+// task 19/48
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number=> number%2===0);
+// const oddNumbers = numbers.filter(number=> number%2!==0);
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
+// task 20/48
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       genres: ["adventure", "history"],
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       genres: ["fiction", "mysticism"],
+//     },
+//     {
+//       title: "Redder Than Blood",
+//       author: "Tanith Lee",
+//       genres: ["horror", "mysticism", "adventure"],
+//     },
+//   ];
+//   // Change code below this line
+//   const allGenres = books.flatMap(book => book.genres);
+//   const uniqueGenres = allGenres.filter((allGenr, index, allGenres) => allGenres.indexOf(allGenr)===index );
+
+//   console.log(uniqueGenres);
+
+// task 22/48
+
+const users = [
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      eyeColor: "blue",
+      friends: ["Sharron Pace"],
+      isActive: false,
+      balance: 2811,
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      eyeColor: "blue",
+      friends: ["Briana Decker", "Sharron Pace"],
+      isActive: true,
+      balance: 3821,
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      eyeColor: "green",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      isActive: false,
+      balance: 3793,
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      eyeColor: "green",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      isActive: true,
+      balance: 2278,
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      eyeColor: "blue",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      isActive: true,
+      balance: 3951,
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      eyeColor: "brown",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      isActive: false,
+      balance: 1498,
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      eyeColor: "brown",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      isActive: true,
+      balance: 2764,
+      gender: "female"
+    }
+  ];
+
+//   const getUsersWithEyeColor = (users, color) => {
+//     return users.filter(user => user.eyeColor===color);
+   
+//    };
+
+//    console.log(getUsersWithEyeColor(users, "blue"));
+
+// task 24/48
+
+// const getUsersWithFriend = (users, friendName) => {
+//     return users.filter(user => {
+//         user.friends
+//     console.log(user.friends.includes(friendName))} )
+    
+    
+//  };
+  
+//  getUsersWithFriend(users, "Briana Decker");
+
+// task 25/48
+
+// const getFriends = (users) => {
+//     // const friends = users.flatMap(user => user.friends);
+//     // console.log(friends);
+
+//     console.log (users.flatMap(user => user.friends).filter((friend, index, friends) => friends.indexOf(friend)===index))
+//  };
+
+//  getFriends(users);
+
+
+  
+
+
+
