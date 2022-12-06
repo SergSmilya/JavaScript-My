beeafeddb4f26b1b7c4603a2bd83755f6afa36fe
SergSmilya <sergsmilichenko@gmail.com>
2022-12-06 20:05:27 +0200

    hw_5 19/20

--- js/hw_5.js
+++ js/hw_5.js
@@ -99,27 +99,40 @@
 
 // task 11/20
 
-class StringBuilder {
-  constructor(initialValue) {
-    this.initialValue = initialValue;
-  }
-  //   getValue() {
-  //     return (this.value = `${this.value}`);
-  //   }
-  //   padEnd(str) {
-  //     return (this.value = `${this.value}${str}`);
-  //   }
-  //   padStart(str) {
-  //     return (this.value = `${str}${this.value}`);
-  //   }
-  //   padBoth(str) {
-  //     return (this.value = `${str}${this.value}${str}`);
-  //   }
-}
+// class StringBuilder {
+//   constructor(initialValue = {}) {
+//     this.initialValue = initialValue;
+//     this.value = initialValue;
+//   }
+//   getValue() {
+//     return (this.value = `${this.value}`);
+//   }
+// padEnd(str) {
+//   return (this.value = `${this.value}${str}`);
+// }
+// padStart(str) {
+//   return (this.value = `${str}${this.value}`);
+// }
+// padBoth(str) {
+//   return (this.value = `${str}${this.value}${str}`);
+// }
+
+// 2 спосіб вирішення (оператор + і прибрати return)
+
+//   padEnd(str) {
+//     this.value += str;
+//   }
+//   padStart(str) {
+//     this.value = str + this.value;
+//   }
+//   padBoth(str) {
+//     this.value = str + this.value + str;
+//   }
+// }
 
 // Change code above this line
-const builder = new StringBuilder(".");
-console.log(builder);
+// const builder = new StringBuilder(".");
+// console.log(builder);
 // console.log(builder.getValue()); // "."
 // builder.padStart("^");
 // console.log(builder.getValue()); // "^."
@@ -127,3 +140,121 @@ console.log(builder);
 // console.log(builder.getValue()); // "^.^"
 // builder.padBoth("=");
 // console.log(builder.getValue()); // "=^.^="
+
+// task 16/20
+
+// class Car {
+//   // Change code below this line
+//   static MAX_PRICE = 50000;
+
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE) {
+//       this.#price = newPrice;
+//     }
+//   }
+
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// task 17/20
+
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+//   static checkPrice(price) {
+//     if (price > Car.#MAX_PRICE) {
+//       return "Error! Price exceeds the maximum";
+//     } else {
+//       return "Success! Price is within acceptable limits";
+//     }
+//   } // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// task 20/20
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Change code below this line
+
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+  static blacklistedEmails = [];
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+
+  set blacklist(email) {
+    return Admin.blacklistedEmails.push(email);
+  }
+
+  // set isBlacklisted(email) {
+  //   return blacklistedEmails.every((element) => {
+  //     if (element === email) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   });
+  // }
+
+  // Change code above this line
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
